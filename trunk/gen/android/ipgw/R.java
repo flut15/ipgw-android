79076363327e885fc158e26d5ab29f1aa425de0c
pkuccnet <pkuccnet@89e5b6d8-bffa-0210-d2f1-c1f31e61dd9b>
2011-05-11 11:36:06 +0000

    0.1

--- trunk/gen/android/ipgw/R.java
+++ trunk/gen/android/ipgw/R.java
@@ -20,7 +20,7 @@ public final class R {
         public static final int linearLayout1=0x7f050006;
         public static final int passwd_text=0x7f050003;
         public static final int password=0x7f050004;
-        public static final int textView1=0x7f050005;
+        public static final int status=0x7f050005;
         public static final int title=0x7f050000;
         public static final int userid=0x7f050002;
         public static final int userid_text=0x7f050001;
--- trunk/src/android/ipgw/ipgw.java
+++ trunk/src/android/ipgw/ipgw.java
@@ -7,8 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.*;
+import java.net.*;
+import java.security.cert.*;
+import javax.net.ssl.*;
 
 public class ipgw extends Activity {
+    private int port = 5428;
+    private String host = "its.pku.edu.cn";
+    private String page = "/ipgatewayofpku";
+    private String argument = "";
+	
+    private TextView status;
+    private TextView userid;
+    private TextView passwd;
+	
+    private String USER_ID = "";//用户名
+    private String PASSWORD = "";//密码
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,30 +37,135 @@ public class ipgw extends Activity {
         Button button_connect = (Button)findViewById(R.id.connect);
         Button button_connect_all = (Button)findViewById(R.id.connect_all);
         Button button_disconnect = (Button)findViewById(R.id.disconnect);
+        status = (TextView)findViewById(R.id.status);
+        userid = (TextView)findViewById(R.id.userid);
+        passwd = (TextView)findViewById(R.id.password);
         
         button_connect.setOnClickListener(listener_connect);
         button_connect_all.setOnClickListener(listener_connect_all);
         button_disconnect.setOnClickListener(listener_disconnect);
+        
+        System.out.print("Program Start.");
     }
+    public void onDestroy() {
+    	super.onDestroy();
+    }
+    
     private OnClickListener listener_connect = new OnClickListener()
     {
     	public void onClick(View v)
     	{
-    		
+    		String result;
+    		result = connect();
+    		status.setText("status : "+USER_ID+" connect.\n"+result);
     	}
     };
     private OnClickListener listener_connect_all = new OnClickListener()
     {
     	public void onClick(View v)
     	{
-    		
+    		String result;
+    		result = connect_all();
+    		status.setText("status : "+USER_ID+" connect all.\n"+result);
     	}
     };
     private OnClickListener listener_disconnect = new OnClickListener()
     {
     	public void onClick(View v)
     	{
-    		
+    		String result;
+    		result = disconnect();
+    		status.setText("status : "+USER_ID+" disconnect.\n"+result);
     	}
     };
+    // 连接免费地址
+    private String connect() {
+    	String result="";
+    	try{
+    		USER_ID = String.valueOf(userid.getText());
+    		PASSWORD = String.valueOf(passwd.getText());
+    		argument = "?uid="+USER_ID+"&password="+PASSWORD+"&timeout=1&range=2&operation=connect";
+    		result = https_request();
+    	}catch (Exception e){
+    		status.setText("error : "+e.getMessage());
+    	}
+    	return result;
+    };
+    // 连接收费地址
+    private String connect_all() {
+    	String result="";
+    	try{
+    		USER_ID = String.valueOf(userid.getText());
+    		PASSWORD = String.valueOf(passwd.getText());
+    		argument = "?uid="+USER_ID+"&password="+PASSWORD+"&timeout=1&range=1&operation=connect";
+    		result = https_request();
+    	} catch (Exception e) {
+    		status.setText("error : "+e.getMessage());
+    	}
+    	return result;
+    };
+    // 断开连接
+    private String disconnect() {
+    	String result="";
+    	try{
+    		USER_ID = String.valueOf(userid.getText());
+    		PASSWORD = String.valueOf(passwd.getText());
+    		argument = "?uid="+USER_ID+"&password="+PASSWORD+"&timeout=1&range=2&operation=disconnectall";
+    		result = https_request();
+    	} catch (Exception e) {
+    		status.setText("error : "+e.getMessage());
+    	}
+    	return result;
+    };
+    // https请求
+    private String https_request() throws Exception{
+    	String result=""; 
+	    // 受访的页面
+    	String url = page + argument;
+	    // 自定义的管理器
+	    X509TrustManager xtm = new MyTrustManager();
+	    TrustManager mytm[] = { xtm };
+	    // 得到上下文
+	    SSLContext ctx = SSLContext.getInstance("SSL");
+	    // 初始化
+	    ctx.init(null, mytm, null);
+	    // 获得工厂
+	    SSLSocketFactory factory = ctx.getSocketFactory();
+	    // 从工厂获得Socket连接
+	    Socket socket = factory.createSocket(host, port);
+	    // 剩下的就和普通的Socket操作一样了
+	    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+	    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	    out.write("GET " + url + " HTTP/1.0\n\n");
+	    out.flush();
+	    // System.out.println("start   work!");
+	    String line;
+	    StringBuffer sb = new StringBuffer();
+	    while ((line = in.readLine()) != null) {
+	    	sb.append(line + "\n");
+	    }       
+	    out.close();
+	    in.close();
+	    // System.out.println(sb.toString());
+	    result = sb.toString();
+    	return result;
+    }
+    // 自定义认证管理类
+    class MyTrustManager implements X509TrustManager {
+    	MyTrustManager() {
+    		// 证书初始化操作
+    	}
+    	public void checkClientTrusted(X509Certificate chain[], String authType)
+    	throws CertificateException {
+    		// 检查客户端可信任状态
+    	}
+    	public void checkServerTrusted(X509Certificate chain[], String authType)
+    	throws CertificateException {
+    		// 检查服务器端可信任状态
+    	}
+    	// 返回接受的发行商数组
+    	public X509Certificate[] getAcceptedIssuers() {
+    		return null;
+    	}
+    }
 }
